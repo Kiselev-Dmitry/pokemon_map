4952db8dba79475a5795328cb8622ac7a9d6f8d8
Kiselev-Dmitry <kdv150275@gmail.com>
2023-09-14 21:49:16 +0300

    show Pokemon name in admin UI.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-class Post(models.Model):
-    name = models.CharField(max_length=20, verbose_name="Имя покемона")
-    picture = models.ImageField(verbose_name="Изображение покемона")
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Имя покемона", null=True)
+    picture = models.ImageField(verbose_name="Изображение покемона", null=True)
+
+    def __str__(self):
+        return f'{self.name}'
